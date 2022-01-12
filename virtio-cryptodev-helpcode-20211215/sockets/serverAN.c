fc945d7845873ee6359fea567f86be41a83be122
andrewevag <andrewevag@gmail.com>
2022-01-13 01:35:55 +0200

    b4 i switch to laptop

--- virtio-cryptodev-helpcode-20211215/sockets/serverAN.c
+++ virtio-cryptodev-helpcode-20211215/sockets/serverAN.c
@@ -13,11 +13,10 @@
 #include <signal.h>
 #include <unistd.h>
 #include <netdb.h>
-
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-
+#include <fcntl.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
@@ -52,13 +51,95 @@ ssize_t insist_write(int fd, const void *buf, size_t cnt)
 	return orig_cnt;
 }
 
+
+int splitToWords(char* str, int length,char** words, int maxwords){
+	// "delimiters are \n and space"
+	int numofwords = 0;
+	words[numofwords++] = str; 
+	for(int i = 0; i < length; i++)
+	{	
+		if(str[i] == ' ' || str[i] == '\n' )
+		{
+			str[i++] = 0;
+			while(str[i] == ' ' || str[i] == '\n'){
+				i++;
+			}
+			words[numofwords++] = &(str[i]);
+			if(numofwords >= maxwords){
+				return numofwords-1;
+			}
+		}
+	}
+	return numofwords-1;
+}
+
 /*
  * Arguments are commands
+ * 
  * c -> channel create channel parameter channel name
  * a -> add user to channel -channel -name
  * s -> send message to channel \n -> msg
  * r -> read channel request read of all the channel.
+ * 
+ * DATA PACKET WILL BE
+ * USERNAME
+ * COMMAND ARGUMENT
+ * data
+ * |
+ * 
  */
+
+int errorcheck(int val, int targetval, char* msg)
+{
+	if(val == targetval)
+	{
+		perror(msg);
+		exit(EXIT_FAILURE);
+	}
+	return val;
+}
+int ANAFILE;
+void AN_protocol_setup()
+{
+	//create a file to manage channel access
+	ANAFILE = errorcheck(open("ANaccess", O_CREAT | O_TRUNC | O_APPEND, S_IRUSR | S_IWUSR) < 0, 1,"failed to create the access file\n");
+	
+
+}
+
+int AN_protocol_execute(char** words,int numofwords)
+{
+	printf("got here with %d\n", numofwords);
+	if(numofwords < 4)
+	{
+		printf("wrong command\n");
+		return -1;
+	}
+	if(strcmp(words[1], "C") == 0)
+	{
+		//create file channel and add user as andreas
+		insist_write(ANAFILE, )
+	}
+	else if(strcmp(words[1], "A") == 0)
+	{
+
+	}
+	else if(strcmp(words[1], "S") == 0)
+	{
+
+	}
+	else if(strcmp(words[1], "R") == 0)
+	{
+
+	}
+	else
+	{
+		printf("wrong command\n");
+		return -1;
+	}
+	return 0;
+
+}
 
 
 
@@ -116,33 +197,50 @@ int main(void)
 		}
 		fprintf(stderr, "Incoming connection from %s:%d\n",
 			addrstr, ntohs(sa.sin_port));
+		
+
+
+		//Variables used for handling a customer
+		for(int i = 0; i < MAX_MSIZE; i++) buf[i] = 0;
+		char* words[MAX_MSIZE];
 
 		/* We break out of the loop when the remote peer goes away */
 		for (;;) {
 			// | msg ends at this character.
 			//maximum message length 
 			int readbytes = 0;
-			for(int i = 0; i < MAX_MSIZE; i++) buf[i] = 0;
-			while(strchr(buf, '|') != NULL){
+			
+			while(strchr(buf, '|') == NULL){
 				n = read(newsd, buf+readbytes, sizeof(buf));
 				readbytes+=n;
 				if (n <= 0) {
-					if (n < 0)
+					if (n < 0){
 						perror("read from remote peer failed");
-					else
+						goto exit;
+					}
+					else{
 						fprintf(stderr, "Peer went away\n");
-					break;
+						goto exit;
+					}
+					
 				}
+				
 			}
-			printf("read msg :\n %s\n", buf);
+			int numofwords = splitToWords(buf, strlen(buf), words, MAX_MSIZE);
+
+			//IMPLEMENTING PROTOCOL...
+			AN_protocol_execute(words, numofwords);
+
 			
 			
-			toupper_buf(buf, n);
-			if (insist_write(newsd, buf, n) != n) {
-				perror("write to remote peer failed");
-				break;
-			}
+			goto exit;
+			// toupper_buf(buf, n);
+			// if (insist_write(newsd, buf, n) != n) {
+			// 	perror("write to remote peer failed");
+			// 	break;
+			// }
 		}
+exit:
 		/* Make sure we don't leak open files */
 		if (close(newsd) < 0)
 			perror("close");
