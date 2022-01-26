2954b2921a1798d5daa35be1c5abefefddbba786
NicoleMp2 <el18604@mail.ntua.gr>
2022-01-26 02:30:05 +0200

    errorcheck

---
+++ virtio-cryptodev-helpcode-20211215/sockets/crypto/encrypt.c
@@ -0,0 +1,57 @@
+#include "socket-common.h"
+#include <crypto/cryptodev.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include "linkedlist.h"
+#include "cryptops.h"
+
+/**
+ * @brief encrypts data
+ * 
+ * @param input data to be ecrypted
+ * @param output encrypted data
+ * @param size size of input/output data
+ */
+void encryption(unsigned char* input, unsigned char* output,int size){
+    int fd = open("/dev/crypto", O_RDWR);
+    errorcheck(fd,-1,"open(/dev/crypto) {encryption}");
+
+    unsigned char key[] = KEY;
+    unsigned char iv[] = IV;
+
+    struct session_op sess;
+    struct crypt_op cryp;
+
+    memset(&sess, 0, sizeof(sess));
+	memset(&cryp, 0, sizeof(cryp));
+
+    /*
+	 * Get crypto session for AES128
+	 */
+	sess.cipher = CRYPTO_AES_CBC;
+	sess.keylen = KEY_SIZE;
+	sess.key = key;
+
+	errorcheck(!ioctl(fd, CIOCGSESSION, &sess),0, "ioctl(CIOCGSESSION) {encryption}" );
+
+    /*
+	 * Encrypt input to output
+	 */
+	cryp.ses = sess.ses;
+	cryp.len = size;
+	cryp.src = input;
+	cryp.dst = output;
+	cryp.iv = iv;
+	cryp.op = COP_ENCRYPT;
+
+	errorcheck(!ioctl(fd, CIOCCRYPT, &cryp),0, "ioctl(CIOCCRYPT) {encryption}" );
+
+    /*Finish crypto session*/
+    errorcheck(!ioctl(fd, CIOCFSESSION, &sess.ses),0,"ioctl(CIOCFSESSION) {encryption}");
+
+    errorcheck(close(fd),-1,"close(/dev/crypto) {encryption}");
+
+}
