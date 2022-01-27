2c748f24a1fab35871dbcff957be954c49de5610
andrewevag <andrewevag@gmail.com>
2022-01-27 14:36:54 +0200

    CIOCGSESSION memory allocates

--- virtio-cryptodev-helpcode-20211215/virtio-cryptodev/guest/crypto-chrdev.c
+++ virtio-cryptodev-helpcode-20211215/virtio-cryptodev/guest/crypto-chrdev.c
@@ -79,7 +79,9 @@ static int crypto_chrdev_open(struct inode *inode, struct file *filp)
 	if ((ret = nonseekable_open(inode, filp)) < 0)
 		goto fail;
 
-	/* Associate this open file with the relevant crypto device. */
+	/* Associate this open file with the relevant crypto device. 
+	 * from the ones we are handling and are open and are in the crypto_driver_data device list.
+	 */
 	crdev = get_crypto_dev_by_minor(iminor(inode));
 	if (!crdev) {
 		debug("Could not find crypto device with %u minor", 
@@ -151,15 +153,28 @@ static long crypto_chrdev_ioctl(struct file *filp, unsigned int cmd,
 {
 	long ret = 0;
 	int err;
-	struct crypto_open_file *crof = filp->private_data;
-	struct crypto_device *crdev = crof->crdev;
-	struct virtqueue *vq = crdev->vq;
+	struct crypto_open_file *crof = filp->private_data;   	//get our open file representation
+	struct crypto_device *crdev = crof->crdev;				//get which device it refers to.
+	struct virtqueue *vq = crdev->vq;						//get the vq of the device which we use to talk to qemu
+															//we need to give the open fd in QEMU because every device
+															//can and will be open multiple times.
 	struct scatterlist syscall_type_sg, output_msg_sg, input_msg_sg,
-	                   *sgs[3];
+	                   *sgs[8]; 
+	struct scatterlist host_fd_sg;
+	//used in CIOCGSESSION
+	struct scatterlist ioctl_cmd_sg, session_key_sg, session_op_sg, host_return_val_sg;
+	
 	unsigned int num_out, num_in, len;
 #define MSG_LEN 100
 	unsigned char *output_msg, *input_msg;
 	unsigned int *syscall_type;
+	
+	//used in CIOCGSESSION
+	unsigned int* ioctl_cmd;
+	struct session_op __user *s;
+	struct session_op* sess;
+	unsigned char* session_key;
+	int* host_return_val;
 
 	debug("Entering");
 
@@ -177,9 +192,15 @@ static long crypto_chrdev_ioctl(struct file *filp, unsigned int cmd,
 	/**
 	 *  These are common to all ioctl commands.
 	 **/
+	//this creates an sg list.
 	sg_init_one(&syscall_type_sg, syscall_type, sizeof(*syscall_type));
 	sgs[num_out++] = &syscall_type_sg;
 	/* ?? */
+	unsigned int* host_fd = kzalloc(sizeof(unsigned int), GFP_KERNEL);
+	*host_fd = crof->host_fd;
+	host_fd_sg = kzalloc(sizeof(*host_fd_sg), GFP_KERNEL);
+	sg_init_one(&host_fd_sg, host_fd, sizeof(*host_fd));
+	
 
 	/**
 	 *  Add all the cmd specific sg lists.
@@ -187,12 +208,59 @@ static long crypto_chrdev_ioctl(struct file *filp, unsigned int cmd,
 	switch (cmd) {
 	case CIOCGSESSION:
 		debug("CIOCGSESSION");
-		memcpy(output_msg, "Hello HOST from ioctl CIOCGSESSION.", 36);
-		input_msg[0] = '\0';
-		sg_init_one(&output_msg_sg, output_msg, MSG_LEN);
-		sgs[num_out++] = &output_msg_sg;
-		sg_init_one(&input_msg_sg, input_msg, MSG_LEN);
-		sgs[num_out + num_in++] = &input_msg_sg;
+		//memcpy(output_msg, "Hello HOST from ioctl CIOCGSESSION.", 36);
+		//input_msg[0] = '\0';
+		//sg_init_one(&output_msg_sg, output_msg, MSG_LEN);
+		//sgs[num_out++] = &output_msg_sg;
+		//sg_init_one(&input_msg_sg, input_msg, MSG_LEN);
+		//sgs[num_out + num_in++] = &input_msg_sg;
+
+		/* implementation of CIOCGSESSION.
+		 * we need to add : 
+		 * 	unsigned int ioctl command.> here CIOCGSESSION 	READ
+		 * 	unsigned char* session_key[];   				READ
+		 * 	struct_session_op session_op.					WRITE
+		 * 	int host_return_val.							WRITE
+		 */
+		ioctl_cmd = kzalloc(sizeof(unsigned int), GFP_KERNEL);
+		*ioctl_cmd = CIOCGSESSION;
+		s = arg; 
+		sess = kzalloc(sizeof(struct session_op), GFP_KERNEL);
+		if(copy_from_user(sess, s, sizeof(struct session_op))){
+			//free memory
+			return -EFAULT;
+		}
+		//here we copy the key to a new buffer so that QEMU can access it because
+		//originally it was stored in userspace buffer that QEMU cannot access.
+		session_key = kzalloc(sizeof(unsigned char) * sess->keylen, GFP_KERNEL);
+		if(copy_from_user(session_key, sess->key, sess->keylen)){
+			//free memory DON'T FORGET.
+			return -EFAULT;
+		}
+		// possibly obsolete.
+		// sess->key = session_key;
+		host_return_val = kzalloc(sizeof(int), GFP_KERNEL);
+		*host_return_val = -1; 
+
+		//put them into sg_lists;
+		//IOCTL_CMD_SG
+		ioctl_cmd_sg = kzalloc(sizeof(*ioctl_cmd_sg), GFP_KERNEL);
+		sg_init_one(&ioctl_cmd_sg, ioctl_cmd, sizeof(*ioctl_cmd));
+		sgs[num_out++ + num_in] = &ioctl_cmd_sg;
+		//SESSION_KEY_SG
+		session_key_sg = kzalloc(sizeof(*session_key_sg), GFP_KERNEL);
+		sg_init_one(&session_key_sg, session_key, sess->keylen);
+		sgs[num_out++ + num_in] = &session_key_sg;
+		//SESSION_OP_SG
+		session_op_sg = kzalloc(sizeof(*session_op_sg), GFP_KERNEL);
+		sg_init_one(&session_op_sg, sess, sizeof(*sess));
+		sgs[num_out + num_in++] = &session_op_sg;
+		//HOST_RETURN_VAL
+		host_return_val_sg = kzalloc(sizeof(*host_return_val_sg), GFP_KERNEL);
+		sg_init_one(&host_return_val_sg, host_return_val, sizeof(*host_return_val));
+		sgs[num_out + num_in++] = &host_return_val_sg;
+
+
 
 		break;
 
@@ -230,11 +298,22 @@ static long crypto_chrdev_ioctl(struct file *filp, unsigned int cmd,
 	 **/
 	/* ?? */
 	/* ?? Lock ?? */
+	if(down_interruptible(&(crdev->sem))){
+		//here we were interrupted or failed to acquire the lock.
+		debug("Failed to acquire lock or was interrupted");
+		return -ERESTARTSYS;
+	}
+
 	err = virtqueue_add_sgs(vq, sgs, num_out, num_in,
-	                        &syscall_type_sg, GFP_ATOMIC);
-	virtqueue_kick(vq);
+	                        &syscall_type_sg, GFP_ATOMIC); 	//the syscall_type_sg is given as primary key.
+	virtqueue_kick(vq);										//wakes up QEMU.
+
+	//u
 	while (virtqueue_get_buf(vq, &len) == NULL)
 		/* do nothing */;
+
+	//probably here we unlock.
+	up(&(crdev->sem));
 
 	debug("We said: '%s'", output_msg);
 	debug("Host answered: '%s'", input_msg);
@@ -272,6 +351,8 @@ int crypto_chrdev_init(void)
 	unsigned int crypto_minor_cnt = CRYPTO_NR_DEVICES;
 	
 	debug("Initializing character device...");
+	
+	//Association with file_operations.
 	cdev_init(&crypto_chrdev_cdev, &crypto_chrdev_fops);
 	crypto_chrdev_cdev.owner = THIS_MODULE;
 	
